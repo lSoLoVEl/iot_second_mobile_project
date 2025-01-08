f9627cc12be6956c128755d82dcec18fd322ccb6
lSoLoVEl <S6519410044@sau.ac.th>
2025-01-08 12:11:36 +0700

    source version 2: create new 2 file

---
+++ lib/about_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class yyy extends StatefulWidget {
+  const yyy({super.key});
+
+  @override
+  State<yyy> createState() => _yyyState();
+}
+
+class _yyyState extends State<yyy> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/home_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class xxx extends StatefulWidget {
+  const xxx({super.key});
+
+  @override
+  State<xxx> createState() => _xxxState();
+}
+
+class _xxxState extends State<xxx> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
